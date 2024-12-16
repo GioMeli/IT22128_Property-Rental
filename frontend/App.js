c546994c7b53dc049473f578ef97e46e78d329eb
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-12-16 22:59:29 +0200

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import PropertyList from './components/PropertyList';
+
+function App() {
+    return (
+        <div>
+            <PropertyList />
+        </div>
+    );
+}
+
+export default App;
 
