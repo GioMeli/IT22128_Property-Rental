19a5b3ed4c99edcdeab4510e3a7aa6a75aebd76c
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-12-16 23:00:32 +0200

    Update PropertyList.js

--- frontend/src/components/PropertyList.js
+++ frontend/src/components/PropertyList.js
@@ -1,1 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PropertyList = () => {
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/properties')
+            .then(response => setProperties(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Properties</h1>
+            <ul>
+                {properties.map(property => (
+                    <li key={property.id}>
+                        {property.name} - {property.location} - ${property.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default PropertyList;
 
