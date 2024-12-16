58c75ff8841bf156b93ca6081464628dfc35a737
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-12-16 23:17:27 +0200

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './AuthContext'; // Το context για αυθεντικοποίηση
+import App from './App';
+import './index.css'; // Αν υπάρχει βασικό CSS
+
+ReactDOM.render(
+    <React.StrictMode>
+        <AuthProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
