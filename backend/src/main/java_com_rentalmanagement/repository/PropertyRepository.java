b55025a2b5545eac8474e85e806328d2ce2a126c
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-12-16 22:49:58 +0200

    Update PropertyRepository.java

--- backend/src/main/java_com_rentalmanagement/repository/PropertyRepository.java
+++ backend/src/main/java_com_rentalmanagement/repository/PropertyRepository.java
@@ -1,1 +1,8 @@
+package com.rentalmanagement.repository;
+
+import com.rentalmanagement.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+}
 
