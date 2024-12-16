4a8c6788d0e199310ff7b489859eaff707a0fc7d
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-12-16 22:44:48 +0200

    Update PropertyController.java

--- backend/src/main/java_com_rentalmanagement/controller/PropertyController.java
+++ backend/src/main/java_com_rentalmanagement/controller/PropertyController.java
@@ -1,1 +1,29 @@
+package com.rentalmanagement.controller;
+
+import com.rentalmanagement.model.Property;
+import com.rentalmanagement.repository.PropertyRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/properties")
+public class PropertyController {
+
+    private final PropertyRepository propertyRepository;
+
+    public PropertyController(PropertyRepository propertyRepository) {
+        this.propertyRepository = propertyRepository;
+    }
+
+    @GetMapping
+    public List<Property> getAllProperties() {
+        return propertyRepository.findAll();
+    }
+
+    @PostMapping
+    public Property createProperty(@RequestBody Property property) {
+        return propertyRepository.save(property);
+    }
+}
 
