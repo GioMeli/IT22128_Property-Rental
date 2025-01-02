e4a162681a41514e494653cf079fa61dda109e1d
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-01-02 15:27:28 +0200

    data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+
+INSERT INTO roles (id, name) VALUES (1, 'ADMIN'), (2, 'USER');
 INSERT INTO user (id, username, email, password, role) VALUES (1, 'admin', 'admin@example.com', 'password', 'ADMIN');
 INSERT INTO property (id, name, location, price) VALUES (1, 'Apartment 1', 'Athens', 500.00);
 INSERT INTO property (id, name, location, price) VALUES (2, 'Apartment 2', 'Thessaloniki', 700.00);
