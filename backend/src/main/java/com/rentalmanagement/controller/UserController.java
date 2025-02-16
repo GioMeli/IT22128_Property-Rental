a2cd93146e1df133faf601c2f4d5d5e875a5bc13
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:16:07 +0200

    Update UserController.java

--- backend/src/main/java/com/rentalmanagement/controller/UserController.java
+++ backend/src/main/java/com/rentalmanagement/controller/UserController.java
@@ -1,1 +1,28 @@
+package com.rentalmanagement.controller;
 
+import com.rentalmanagement.model.User;
+import com.rentalmanagement.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
