490daaf505d04c828839c64dcafc73041dead519
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:17:44 +0200

    Delete backend/src/main/java/repository directory

--- backend/src/main/java/repository/PropertyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentalmanagement.repository;
-
-import com.rentalmanagement.model.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
--- backend/src/main/java/repository/RentalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentalmanagement.repository;
-
-import com.rentalmanagement.model.RentalRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalRepository extends JpaRepository<RentalRequest, Long> {
-}
