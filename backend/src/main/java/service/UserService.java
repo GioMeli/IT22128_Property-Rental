68825e8d068c37af11eca2193ebfcbca40528dc4
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 21:03:40 +0200

    UserService.java

---
+++ backend/src/main/java/service/UserService.java
@@ -0,0 +1,18 @@
+package com.rentalmanagement.service;
+
+import com.rentalmanagement.model.User;
+import com.rentalmanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    // Other methods
+}
