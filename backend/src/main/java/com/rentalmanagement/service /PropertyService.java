b5d9730e5906354368ac6bb5f9ed330c66a53e48
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:10:00 +0200

    Update PropertyService.java

--- backend/src/main/java/com/rentalmanagement/service /PropertyService.java
+++ backend/src/main/java/com/rentalmanagement/service /PropertyService.java
@@ -1,1 +1,20 @@
+package com.rentalmanagement.service;
 
+import com.rentalmanagement.model.Property;
+import com.rentalmanagement.repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PropertyService {
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    public List<Property> findAll() {
+        return propertyRepository.findAll();
+    }
+
+    // Other methods
+}
