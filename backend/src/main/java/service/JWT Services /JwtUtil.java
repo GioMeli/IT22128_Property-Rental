3ba7f3ab798b4b1bba72f62303158eacbc826ca9
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 21:01:30 +0200

    JwtUtil.java

---
+++ backend/src/main/java/service/JWT Services /JwtUtil.java
@@ -0,0 +1,25 @@
+package com.rentalmanagement.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "your_secret_key";
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 ώρες
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
