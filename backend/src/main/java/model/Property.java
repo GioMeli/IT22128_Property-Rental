06ed7a2474550c8e9a40bbe5c38fec1192fa7aa3
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 20:54:24 +0200

    Property.java

---
+++ backend/src/main/java/model/Property.java
@@ -0,0 +1,15 @@
+package com.rentalmanagement.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private double price;
+
+    // Getters and Setters
+}
