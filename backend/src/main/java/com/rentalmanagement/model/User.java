06c386c8f349987685187ffeb54db1021b96b5b9
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:34:51 +0200

    Update User.java

--- backend/src/main/java/com/rentalmanagement/model/User.java
+++ backend/src/main/java/com/rentalmanagement/model/User.java
@@ -1,23 +1,34 @@
 package com.rentalmanagement.model;
 
-import jakarta.persistence.*;
-import java.util.Set;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
-    private String email;
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    public enum Role {
-        USER, ADMIN
+    // Getters and setters
+    public Long getId() {
+        return id;
     }
-
-    // Getters and Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
