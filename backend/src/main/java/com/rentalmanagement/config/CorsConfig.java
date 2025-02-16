45d61f6b136ef089a858cb80e656308211295fe8
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:29:47 +0200

    Update and rename configCorsConfig.java to CorsConfig.java

--- backend/src/main/java/com/rentalmanagement/config/CorsConfig.java
+++ backend/src/main/java/com/rentalmanagement/config/CorsConfig.java
@@ -13,10 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Allow frontend requests
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
     }
 }
+
