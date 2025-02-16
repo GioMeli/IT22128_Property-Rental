47bd1f088c26b8c3e496092bed625e74af3c11de
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:37:13 +0200

    Update UserRepository.java

--- backend/src/main/java/com/rentalmanagement/repository/UserRepository.java
+++ backend/src/main/java/com/rentalmanagement/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.rentalmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Custom query to find a user by username
     User findByUsername(String username);
 }
