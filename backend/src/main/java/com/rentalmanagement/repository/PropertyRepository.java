dce734d277d35399f8007ccb21819e030a9f3d1f
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:37:34 +0200

    Update PropertyRepository.java

--- backend/src/main/java/com/rentalmanagement/repository/PropertyRepository.java
+++ backend/src/main/java/com/rentalmanagement/repository/PropertyRepository.java
@@ -4,4 +4,6 @@ import com.rentalmanagement.model.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    // You can add custom queries if needed, e.g.:
+    // List<Property> findByAvailable(boolean available);
 }
