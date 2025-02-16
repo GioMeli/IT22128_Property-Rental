853ddf102c8eb1ad82edd9b0730c8dcfadbf55e9
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-16 22:36:10 +0200

    Update RentalRepository.java

--- backend/src/main/java/com/rentalmanagement/repository/RentalRepository.java
+++ backend/src/main/java/com/rentalmanagement/repository/RentalRepository.java
@@ -1,7 +1,11 @@
 package com.rentalmanagement.repository;
 
-import com.rentalmanagement.model.RentalRequest;
+import com.rentalmanagement.model.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface RentalRepository extends JpaRepository<RentalRequest, Long> {
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    List<Rental> findByUserId(Long userId);
+    List<Rental> findByStatus(String status);
+    List<Rental> findByPropertyId(Long propertyId);
 }
