8aa76f4877692e5fa2788feb2f83fe59ef2f64f9
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-17 23:54:48 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,3 +10,19 @@ function App() {
 }
 
 export default App;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
