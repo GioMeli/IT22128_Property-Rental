f342a646c6c497dd1d866b3ec09d0a51c53d0d23
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-18 10:56:40 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,4 @@
-import React from 'react';
-import PropertyList from './components/PropertyList';
-
-function App() {
-    return (
-        <div>
-            <PropertyList />
-        </div>
-    );
-}
-
-export default App;
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -42,5 +15,4 @@ function App() {
 }
 
 export default App;
-
 
