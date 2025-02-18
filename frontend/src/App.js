81391348f506312985803df0bbd149314f1d300d
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-18 22:41:55 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
+import Home from "./Home"; // <-- Make sure you have a Home.js component
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Default route => Login page */}
         <Route path="/" element={<Login />} />
+
+        {/* Signup page */}
         <Route path="/signup" element={<Signup />} />
+
+        {/* Home page => after login */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
