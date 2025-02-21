d2f0c3f6011c168e5dd56f5040b9662a27364e7e
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-02-21 14:03:10 +0200

    AuthController.java

--- backend/src/main/java/com/rentalmanagement/controller/AuthController.java
+++ backend/src/main/java/com/rentalmanagement/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.rentalmanagement.controller;
 
-import com.rentalmanagement.model.User;
 import com.rentalmanagement.repository.UserRepository;
 import com.rentalmanagement.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
